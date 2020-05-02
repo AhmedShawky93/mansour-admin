c9f73eb494848b48ee763f1d6ce8590cd3e97d00
islam-abdallah <engislamabdoo1995@gmail.com>
2020-05-03 00:01:09 +0200

    update

--- src/app/pages/order-mangament/orders/orders.component.ts
+++ src/app/pages/order-mangament/orders/orders.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { OrdersService } from '@app/pages/services/orders.service';
-import { CategoryService } from '@app/pages/services/category.service';
-import { ToastrService } from 'ngx-toastr';
-import { MatInput } from '@angular/material';
+import { OrderStatesService } from "./../../services/order-states.service";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { OrdersService } from "@app/pages/services/orders.service";
+import { CategoryService } from "@app/pages/services/category.service";
+import { ToastrService } from "ngx-toastr";
+import { MatInput } from "@angular/material";
 
-import * as moment from 'moment';
+import * as moment from "moment";
 
-import {Title} from '@angular/platform-browser';
-import { Subject } from 'rxjs';
-import { tap } from 'rxjs/operators';
-import { AuthService } from '@app/shared/auth.service';
-import { environment } from '@env/environment';
+import { Title } from "@angular/platform-browser";
+import { Subject } from "rxjs";
+import { tap } from "rxjs/operators";
+import { AuthService } from "@app/shared/auth.service";
+import { environment } from "@env/environment";
 
 declare var jquery: any;
 declare var $: any;
 
-
 @Component({
-  selector: 'app-orders',
-  templateUrl: './orders.component.html',
-  styleUrls: ['./orders.component.css']
+  selector: "app-orders",
+  templateUrl: "./orders.component.html",
+  styleUrls: ["./orders.component.css"],
 })
 export class OrdersComponent implements OnInit {
   loading: boolean;
@@ -29,7 +29,7 @@ export class OrdersComponent implements OnInit {
   search: boolean;
   listFilter: string;
   viewFilter: string;
-  @ViewChild('mdate', { read: MatInput }) input: MatInput;
+  @ViewChild("mdate", { read: MatInput }) input: MatInput;
 
   showDelete: any;
   show = false;
@@ -41,7 +41,7 @@ export class OrdersComponent implements OnInit {
   sub_categories: any[];
 
   order: any;
-
+  productTerm: "";
   currentOrder: any;
 
   availableDeliverers: any[];
@@ -49,105 +49,104 @@ export class OrdersComponent implements OnInit {
   product_list: any[];
 
   filter = {
-    term: '',
-    state_id: '',
-    date_from: '',
-    date_to: '',
-    hide_scheduled: 1
+    term: "",
+    state_id: "",
+    date_from: "",
+    date_to: "",
+    hide_scheduled: 1,
   };
 
   p = 1;
 
   newAmount;
 
-  searchTerm = '';
+  searchTerm = "";
 
   total: number = 0;
 
-  filter$ = new Subject;
+  filter$ = new Subject();
   exportUrl: string;
   exportProductsUrl: string;
   productsUrl: string;
+  orderStatus: any;
+  orderSubStatus: any;
 
-  constructor(private ordersService: OrdersService,
-     private catService: CategoryService, private toasterService: ToastrService, private titleService: Title, private auth: AuthService) {
-    this.titleService.setTitle('Orders');
+  constructor(
+    private ordersService: OrdersService,
+    private catService: CategoryService,
+    private toasterService: ToastrService,
+    private titleService: Title,
+    private auth: AuthService,
+    private orderStatesService: OrderStatesService
+  ) {
+    this.titleService.setTitle("Orders");
   }
 
   ngOnInit() {
-
-
-
-
-    $('.payment-open').on('click', function () {
-      $('.payment-area').slideToggle(100);
+    this.getOrderStates();
+    $(".payment-open").on("click", function () {
+      $(".payment-area").slideToggle(100);
     });
 
-
-    $('.tb-processing').on('click', '.color-yellow', function () {
-      $('#view-deactive').toggleClass('open-view-vindor-types');
+    $(".tb-processing").on("click", ".color-yellow", function () {
+      $("#view-deactive").toggleClass("open-view-vindor-types");
     });
 
-    $('.tb-processing').on('click', '.color-purple', function () {
-      $('#view-deactive').toggleClass('open-view-vindor-types');
+    $(".tb-processing").on("click", ".color-purple", function () {
+      $("#view-deactive").toggleClass("open-view-vindor-types");
     });
 
-    $('.tb-assign').on('click', ' .bg-blue', function () {
-      $('#delevary1').slideToggle(100);
+    $(".tb-assign").on("click", " .bg-blue", function () {
+      $("#delevary1").slideToggle(100);
     });
 
-    $('.tb-delivaring').on('click', ' .color-blue', function () {
-      $('#view-delivaring').toggleClass('open-view-vindor-types');
+    $(".tb-delivaring").on("click", " .color-blue", function () {
+      $("#view-delivaring").toggleClass("open-view-vindor-types");
     });
-
 
     // $(".tb-assign").on("click", " .bg-blue", function () {
     //   $("#delevary1").slideToggle(100)
     // });
 
-    $('.delevaryman-popup .close').on('click', function () {
-      $('#delevary1').slideUp(100);
+    $(".delevaryman-popup .close").on("click", function () {
+      $("#delevary1").slideUp(100);
     });
 
-    $('.add-item-btn').on('click', function () {
-      $('.add-prod').slideToggle(100);
+    $(".add-item-btn").on("click", function () {
+      $(".add-prod").slideToggle(100);
     });
 
-    $('.toggle-delevary').on('click', function () {
-      $('#delevary2').slideToggle(100);
+    $(".toggle-delevary").on("click", function () {
+      $("#delevary2").slideToggle(100);
     });
 
-
-    $('.toggle-delevary2').on('click', function () {
-      $('#delevary3').slideToggle(100);
+    $(".toggle-delevary2").on("click", function () {
+      $("#delevary3").slideToggle(100);
     });
 
-
-    $('#delevary2 .close').on('click', function () {
-      $('#delevary2').slideUp(100);
+    $("#delevary2 .close").on("click", function () {
+      $("#delevary2").slideUp(100);
     });
 
-
-    $('#delevary3 .close').on('click', function () {
-      $('#delevary3').slideUp(100);
+    $("#delevary3 .close").on("click", function () {
+      $("#delevary3").slideUp(100);
     });
 
     // for close only
-    $('#view-deactive').on('click', '#close-vindors4', function () {
-      $('#view-deactive').removeClass('open-view-vindor-types');
+    $("#view-deactive").on("click", "#close-vindors4", function () {
+      $("#view-deactive").removeClass("open-view-vindor-types");
     });
 
-    $('#close-vindors6').on('click', function () {
-      $('#view-delivaring').removeClass('open-view-vindor-types');
+    $("#close-vindors6").on("click", function () {
+      $("#view-delivaring").removeClass("open-view-vindor-types");
     });
 
-    $('.payment-area .close').on('click', function () {
-      $('.payment-area').slideUp();
+    $(".payment-area .close").on("click", function () {
+      $(".payment-area").slideUp();
     });
 
-
-    $('.add-prod .close').on('click', function () {
-      $('.add-prod').slideUp();
+    $(".add-prod .close").on("click", function () {
+      $(".add-prod").slideUp();
     });
 
     // this.ordersService.getOrders(1)
@@ -156,39 +155,52 @@ export class OrdersComponent implements OnInit {
     //     this.total = response.data.total;
     //   });
 
-    this.filter$.debounceTime(400)
-      .pipe(tap(e => this.loading = true))
-      .switchMap(filter => this.filterOrders())
+    this.filter$
+      .debounceTime(400)
+      .pipe(tap((e) => (this.loading = true)))
+      .switchMap((filter) => this.filterOrders())
       .subscribe((response: any) => {
         this.loading = false;
         this.orders = response.data.orders;
         this.total = response.data.total;
-        console.log(this.total)
+        console.log(this.total);
         if (this.total === 0) {
           this.p = 1;
         }
+        this.orders.forEach(element => {
+            this.selectStatus(element.state_id)
+        });
       });
 
     this.filter$.next(this.filter);
 
-    this.catService.getCategories()
-      .subscribe((response: any) => {
-        this.categories = response.data;
-      });
+    this.catService.getCategories().subscribe((response: any) => {
+      this.categories = response.data;
+    });
 
     let token = this.auth.getToken();
     this.exportUrl = environment.api + "/admin/orders/export?token=" + token;
-    this.productsUrl = environment.api + "/admin/products/export_sales?token=" + token;
+    this.productsUrl =
+      environment.api + "/admin/products/export_sales?token=" + token;
     this.exportProductsUrl = this.productsUrl;
   }
 
+  getOrderStates() {
+    this.orderStatesService.getOrderStatus().subscribe({
+      next: (response: any) => {
+        if (response.code === 200) {
+          this.orderStatus = response.data;
+        }
+      },
+    });
+  }
   clearDateFrom() {
-    this.filter.date_from = '';
+    this.filter.date_from = "";
     this.changePage(1);
   }
 
   clearDateTo() {
-    this.filter.date_to = '';
+    this.filter.date_to = "";
     this.changePage(1);
   }
 
@@ -204,20 +216,23 @@ export class OrdersComponent implements OnInit {
     this.search = true;
     // this.p = p;
     if (this.filter.date_from) {
-      this.filter.date_from = moment(this.filter.date_from).format('YYYY-MM-DD');
+      this.filter.date_from = moment(this.filter.date_from).format(
+        "YYYY-MM-DD"
+      );
     }
 
     if (this.filter.date_to) {
-      this.filter.date_to = moment(this.filter.date_to).format('YYYY-MM-DD');
+      this.filter.date_to = moment(this.filter.date_to).format("YYYY-MM-DD");
     }
     // console.log(this.serialize(this.filter));
-    this.exportProductsUrl = this.productsUrl + "&" + this.serialize(this.filter);
+    this.exportProductsUrl =
+      this.productsUrl + "&" + this.serialize(this.filter);
 
     return this.ordersService.filterOrders(this.filter, this.p);
   }
 
   toggleProductSelect() {
-    $('.add-prod').slideToggle(100);
+    $(".add-prod").slideToggle(100);
   }
 
   toggleShow() {
@@ -237,16 +252,17 @@ export class OrdersComponent implements OnInit {
   }
 
   showAmount(order) {
-    this.orders.map(item => item.showPayment = 0);
+    this.orders.map((item) => (item.showPayment = 0));
     this.newAmount = order.paid_amount;
     order.showPayment = 1;
   }
 
   updatePaidAmount(order, newAmount) {
-    this.ordersService.updatePaidAmount(order.id, { paid_amount: newAmount })
+    this.ordersService
+      .updatePaidAmount(order.id, { paid_amount: newAmount })
       .subscribe((response: any) => {
         if (response.code === 200) {
-          const ind = this.orders.findIndex(item => item.id === order.id);
+          const ind = this.orders.findIndex((item) => item.id === order.id);
 
           if (ind !== -1) {
             this.orders[ind] = response.data;
@@ -257,19 +273,20 @@ export class OrdersComponent implements OnInit {
 
   getDeliverers(order: any) {
     // load available
-    this.orders.map(item => item.showPopup = 0);
-    this.ordersService.getAvailableDeliverers(order.id)
+    this.orders.map((item) => (item.showPopup = 0));
+    this.ordersService
+      .getAvailableDeliverers(order.id)
       .subscribe((response: any) => {
         this.availableDeliverers = response.data;
-        this.viewFilter = '';
-        this.listFilter = '';
+        this.viewFilter = "";
+        this.listFilter = "";
       });
 
     // order.showPopup = !order.showPopup;
   }
 
   selectDeliverer(selectedUser) {
-    this.availableDeliverers.map(user => {
+    this.availableDeliverers.map((user) => {
       if (user.id === selectedUser.id) {
         user.selected = 1;
       } else {
@@ -280,25 +297,23 @@ export class OrdersComponent implements OnInit {
   }
 
   assignDeliverer(order) {
-    const deliverer = this.availableDeliverers.filter(user => user.selected);
+    const deliverer = this.availableDeliverers.filter((user) => user.selected);
 
     if (!deliverer.length) {
       return;
     }
 
-
-    this.ordersService.assignDeliverer(order.id, { deliverer_id: deliverer[0].id })
+    this.ordersService
+      .assignDeliverer(order.id, { deliverer_id: deliverer[0].id })
       .subscribe((response: any) => {
-
         if (response.code === 200) {
-          const ind = this.orders.findIndex(item => item.id === order.id);
+          const ind = this.orders.findIndex((item) => item.id === order.id);
 
           order.showPopup = false;
           order.showViewPopup = false;
 
           order.state_id = response.data.state_id;
         }
-
       });
   }
 
@@ -307,23 +322,21 @@ export class OrdersComponent implements OnInit {
   }
 
   cancelOrder() {
-    this.ordersService.cancelOrder(this.order.id)
-      .subscribe((response: any) => {
+    this.ordersService.cancelOrder(this.order.id).subscribe((response: any) => {
+      if (response.code === 200) {
+        const ind = this.orders.findIndex((item) => item.id === this.order.id);
 
-        if (response.code === 200) {
-          const ind = this.orders.findIndex(item => item.id === this.order.id);
-
-          if (ind !== -1) {
-            this.orders[ind] = response.data;
-          }
-
-          if (this.currentOrder) {
-            this.currentOrder.state_id = 6;
-          }
-
-          $('#removePopUp').modal('hide');
+        if (ind !== -1) {
+          this.orders[ind] = response.data;
         }
-      });
+
+        if (this.currentOrder) {
+          this.currentOrder.state_id = 6;
+        }
+
+        $("#removePopUp").modal("hide");
+      }
+    });
   }
 
   promtReturn(order) {
@@ -331,78 +344,75 @@ export class OrdersComponent implements OnInit {
   }
 
   returnOrder() {
-    this.ordersService.returnOrder(this.order.id)
-      .subscribe((response: any) => {
+    this.ordersService.returnOrder(this.order.id).subscribe((response: any) => {
+      if (response.code === 200) {
+        const ind = this.orders.findIndex((item) => item.id === this.order.id);
 
-        if (response.code === 200) {
-          const ind = this.orders.findIndex(item => item.id === this.order.id);
-
-          if (ind !== -1) {
-            this.orders[ind] = response.data;
-          }
-
-          this.currentOrder.state_id = 7;
-
-          $('#removePopUp3').modal('hide');
+        if (ind !== -1) {
+          this.orders[ind] = response.data;
         }
-      });
+
+        this.currentOrder.state_id = 7;
+
+        $("#removePopUp3").modal("hide");
+      }
+    });
   }
 
   proceedOrder(order) {
-    this.ordersService.proceedOrder(order.id)
-      .subscribe((response: any) => {
+    this.ordersService.proceedOrder(order.id).subscribe((response: any) => {
+      if (response.code === 200) {
+        const ind = this.orders.findIndex((item) => item.id === order.id);
 
-        if (response.code === 200) {
-          const ind = this.orders.findIndex(item => item.id === order.id);
-
-          if (ind !== -1) {
-            this.orders[ind] = response.data;
-          }
-
-          this.currentOrder.state_id = 1;
-          this.currentOrder.deliverer = null;
-        } else {
-          this.toasterService.error(response.message);
+        if (ind !== -1) {
+          this.orders[ind] = response.data;
         }
-      });
+
+        this.currentOrder.state_id = 1;
+        this.currentOrder.deliverer = null;
+      } else {
+        this.toasterService.error(response.message);
+      }
+    });
   }
 
   openSideView(order) {
-    this.ordersService.getOrder(order.id)
-      .subscribe((response: any) => {
-        $('#view-deactive').toggleClass('open-view-vindor-types');
-        this.currentOrder = response.data;
-        this.selectedCategory = '';
-        this.selectedSubcategory = '';
-        this.product_list = [];
-        $('.add-prod').hide();
-      });
+    this.ordersService.getOrder(order.id).subscribe((response: any) => {
+      $("#view-deactive").toggleClass("open-view-vindor-types");
+      this.currentOrder = response.data;
+      this.selectedCategory = "";
+      this.selectedSubcategory = "";
+      this.product_list = [];
+      $(".add-prod").hide();
+    });
   }
 
   loadSubcategories(cat_id) {
-    const index = this.categories.findIndex(item => item.id == cat_id);
+    const index = this.categories.findIndex((item) => item.id == cat_id);
 
     this.sub_categories = this.categories[index].sub_categories;
-
   }
 
   loadProducts(cat_id) {
-    this.catService.getProducts(cat_id)
-      .subscribe((response: any) => {
-        this.product_list = response.data;
-        this.product_list.map(prod => prod.amount = 0);
-      });
-
+    this.catService.getProducts(cat_id).subscribe((response: any) => {
+      this.product_list = response.data;
+      this.product_list.map((prod) => (prod.amount = 0));
+    });
   }
 
   getDeleteCount() {
-    return this.currentOrder ? this.currentOrder.items.filter((item) => item.remove).length : 0;
+    return this.currentOrder
+      ? this.currentOrder.items.filter((item) => item.remove).length
+      : 0;
   }
 
   deleteItems() {
-    const ids = this.currentOrder.items.filter((item) => item.remove).map(item => item.id);
+    const ids = this.currentOrder.items
+      .filter((item) => item.remove)
+      .map((item) => item.id);
 
-    this.ordersService.removeItems(this.currentOrder.id, { item_ids: ids })
+    this.ordersService
+      .removeItems(this.currentOrder.id, { item_ids: ids })
       .subscribe((response: any) => {
         this.currentOrder.items = response.data;
       });
@@ -413,29 +423,31 @@ export class OrdersComponent implements OnInit {
   }
 
   removeAmount(product) {
-    product.amount > 0 ? product.amount-- : product.amount = 0;
+    product.amount > 0 ? product.amount-- : (product.amount = 0);
   }
 
   addProducts() {
-    const items = this.product_list.filter(item => item.amount).map((item) => {
-      return {
-        product_id: item.id,
-        amount: item.amount
-      };
-    });
+    const items = this.product_list
+      .filter((item) => item.amount)
+      .map((item) => {
+        return {
+          product_id: item.id,
+          amount: item.amount,
+        };
+      });
 
     if (items.length) {
-      this.ordersService.addItems(this.currentOrder.id, { items: items })
+      this.ordersService
+        .addItems(this.currentOrder.id, { items: items })
         .subscribe((response: any) => {
           this.currentOrder.items = response.data;
         });
     }
 
-    this.selectedCategory = '';
-    this.selectedSubcategory = '';
+    this.selectedCategory = "";
+    this.selectedSubcategory = "";
     this.product_list = [];
     this.toggleProductSelect();
-
   }
 
   serialize(obj) {
@@ -445,5 +457,16 @@ export class OrdersComponent implements OnInit {
         str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
       }
     return str.join("&");
+  }
+  selectStatus(id) {
+    console.log(id);
+    let index = this.orderStatus.findIndex(
+      (item) => item.id == id
+    );
+    console.log(index);
+    if (index !== -1) {
+      this.orderSubStatus = this.orderStatus[index].sub_states;
+      console.log(this.orderSubStatus);
+    }
   }
 }
