07c0ed3ff2d2d53abddd11b2758650b3974718ba
sherif <sherif.alaa55@gmail.com>
2020-05-11 03:26:00 +0200

    finalizing

--- src/app/shared/delivery-filter.pipe.ts
+++ src/app/shared/delivery-filter.pipe.ts
@@ -13,8 +13,13 @@ export class DeliveryFilterPipe implements PipeTransform {
 
     if (typeof term !== "undefined" && term != "") {
       return items.filter(item => {
-        let districts = item.deliverer_profile.districts.map(d => d.name).join(" ");
-        return item.name.toLowerCase().includes(term.toLowerCase()) || districts.toLowerCase().includes(term.toLowerCase());
+        if (item.deliverer_profile) {
+          let filter = item.deliverer_profile.districts.findIndex(d => d.id == term);
+          return filter !== -1;
+        }
+        return false;
+        // let districts = item.deliverer_profile.districts.map(d => d.name).join(" ");
+        // return item.name.toLowerCase().includes(term.toLowerCase()) || districts.toLowerCase().includes(term.toLowerCase());
       });
 
     } else {
