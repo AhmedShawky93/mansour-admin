b2f50a63270038dd63138772868f94dd9f23ea5b
sherif <sherif.alaa55@gmail.com>
2020-06-15 23:23:00 +0200

    fix

--- src/app/pages/reports/reports.component.ts
+++ src/app/pages/reports/reports.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { SettingService } from "../services/setting.service";
+import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
 
 @Component({
   selector: "app-reports",
@@ -6,10 +8,17 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./reports.component.css"],
 })
 export class ReportsComponent implements OnInit {
+  iframeUrl: SafeResourceUrl;
+  iframeLoaded = false
 
-  constructor(
-  ) {}
+  constructor(private settingsService: SettingService, private sanitizer: DomSanitizer) {}
 
   ngOnInit() {
+    this.settingsService.reports()
+      .subscribe((response: any) => {
+        this.iframeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(response.data.iframeUrl)
+        this.iframeLoaded = true
+        console.log(this.iframeUrl);
+      })
   }
 }
