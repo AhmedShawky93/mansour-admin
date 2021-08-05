2f74db5e87ee7153f06b0da1d7711fd50122a403
nadenagi <37834449+nadenagi@users.noreply.github.com>
2021-08-05 18:03:53 +0200

    fix build error

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,13 +4,9 @@ import { StringFilterPipe } from "../shared/string-filter.pipe";
 import { CustomerService } from "@app/pages/services/customer.service";
 import { LoaderComponent } from "@app/loader/loader.component";
 import { MenuCreatorComponent } from './menu-creator/menu-creator.component';
-// import { AddEditOrderComponent } from './order-mangament/orders/add-edit-order/add-edit-order.component';
-// import { AddEditAddressComponent } from './mangament-users/manage-castomer/add-edit-address/add-edit-address.component';
-// import { AddEditCustomerComponent } from './mangament-users/manage-castomer/add-edit-customer/add-edit-customer.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [MenuCreatorComponent],
   providers: [CustomerService],
 })
 export class PagesModule {}
