e21eb5d44051ca8a55996c9edd08ad88cf089f94
nadenagi <37834449+nadenagi@users.noreply.github.com>
2021-09-02 11:49:41 +0200

    add address in order

--- src/app/pages/order-mangament/orders/add-edit-order/add-edit-order.component.ts
+++ src/app/pages/order-mangament/orders/add-edit-order/add-edit-order.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angu
 import { FormArray, Validators } from '@angular/forms';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AreasService } from '@app/pages/services/areas.service';
 import { CustomerService } from '@app/pages/services/customer.service';
 import { OrdersService } from '@app/pages/services/orders.service';
 import { ProductsService } from '@app/pages/services/products.service';
@@ -32,11 +33,33 @@ export class AddEditOrderComponent implements OnInit, OnChanges {
   deleted_items: any[] = [];
   loading: boolean;
   selectedProductToUpdate: any;
+  cities: any = [];
+  areas: any = [];
 
-  constructor(private customerService: CustomerService, private router: Router, private productService: ProductsService, private ordersService: OrdersService, private toastrService: ToastrService) { }
+  constructor(private citiesService: AreasService, private customerService: CustomerService, private router: Router, private productService: ProductsService, private ordersService: OrdersService, private toastrService: ToastrService) {
+    
+  }
+
+
+  onCitySelected() {
+    let city_id = this.orderForm.controls.address.get('city_id').value;
+
+    if (city_id) {
+      let ind = this.cities.findIndex(c => c.id == city_id);
+
+      if (ind !== -1) {
+        this.areas = this.cities[ind].areas;
+      }
+    }
+  }
 
   ngOnInit() {
     this.setupForm(this.selectedOrder);
+
+    this.citiesService.getCities()
+      .subscribe((response: any) => {
+        this.cities = response.data;
+      })
   }
 
   ngOnChanges() {
@@ -55,6 +78,16 @@ export class AddEditOrderComponent implements OnInit, OnChanges {
       admin_notes: new FormControl(data ? data.admin_notes : ''),
       overwrite_fees: new FormControl(0),
       delivery_fees: new FormControl(data ? data.delivery_fees : ''),
+      has_address: new FormControl(data ? data.has_address : false),
+      address: new FormGroup({
+        name: new FormControl(data ? data.name : ''),
+        address: new FormControl(data ? data.address : ''),
+        city_id: new FormControl(data ? data.city_id : ''),
+        area_id: new FormControl(data ? data.area_id : ''),
+        landmark: new FormControl(data ? data.landmark : ''),
+        floor: new FormControl(data ? data.floor : ''),
+        apartment: new FormControl(data ? data.apartment : ''),
+      }),
     });
 
     if (data) {
@@ -123,6 +156,27 @@ export class AddEditOrderComponent implements OnInit, OnChanges {
         ))
       )
     );
+  }
+
+  updateValidaty() {
+    if (this.orderForm.controls.has_address.value) {
+      this.orderForm.controls.address['controls'].name.setValidators([Validators.required]);
+      this.orderForm.controls.address_id.setValidators([]);
+      this.orderForm.controls.address_id.updateValueAndValidity();
+      this.orderForm.controls.address['controls'].address.setValidators([Validators.required]);
+      this.orderForm.controls.address['controls'].city_id.setValidators([Validators.required]);
+      this.orderForm.controls.address['controls'].area_id.setValidators([Validators.required]);
+      this.orderForm.controls.address['controls'].floor.setValidators([Validators.required]);
+      this.orderForm.controls.address['controls'].apartment.setValidators([Validators.required]);
+    } else {
+      this.orderForm.controls.address_id.setValidators([Validators.required]);
+      this.orderForm.controls.address['controls'].name.setValidators([]);
+      this.orderForm.controls.address['controls'].address.setValidators([]);
+      this.orderForm.controls.address['controls'].city_id.setValidators([]);
+      this.orderForm.controls.address['controls'].area_id.setValidators([]);
+      this.orderForm.controls.address['controls'].floor.setValidators([]);
+      this.orderForm.controls.address['controls'].apartment.setValidators([]);
+    }
   }
 
   updateAmountMax(item, index){
