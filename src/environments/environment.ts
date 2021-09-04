df1a4c6439b68ad0cf8f41dd5ce60d8d10d37c0b
shirt <shirt@soleeklab.com>
2021-09-04 16:06:45 +0200

    end point from file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-// import { environmentVariables as variables } from './enviromentalVariables';
+ import { environmentVariables as variables } from './enviromentalVariables';
 
 var variables=JSON.parse(localStorage.getItem("systemConfig"));
 
