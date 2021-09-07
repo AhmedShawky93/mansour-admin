744b6ddb4fb784e9521793bf2a90721d8123b472
islam-abdallah <islam@soleeeklab.com>
2021-09-07 12:07:35 +0200

    fix

--- src/app/pages/dynamic-settings/dynamic-settings.component.ts
+++ src/app/pages/dynamic-settings/dynamic-settings.component.ts
@@ -93,7 +93,6 @@ export class DynamicSettingsComponent implements OnInit, AfterViewInit, AfterCon
 
   createDynamicForm() {
     this.componentForm = this.formBuilder.group(this.createMainGroupsControls());
-
   }
 
   createMainGroupsControls() {
@@ -114,7 +113,6 @@ export class DynamicSettingsComponent implements OnInit, AfterViewInit, AfterCon
 
 
       this.formGroups.push(mainGroups[ele.key]);
-      // }
 
     });
 
@@ -127,7 +125,7 @@ export class DynamicSettingsComponent implements OnInit, AfterViewInit, AfterCon
       order: new FormControl(data ? data.order : ''),
       type: new FormControl(data ? data.type : ''),
       name: new FormControl(data ? data.name : ''),
-      value: new FormControl(data ? data.value : '', [data.required ? Validators.required : null]),
+      value: new FormControl(data ? data.value : '', [data.required ? Validators.required : Validators.nullValidator]),
       options: new FormControl(data ? data.options : ''),
       group: new FormControl(data ? data.group : ''),
       slug: new FormControl(data ? data.slug : ''),
@@ -192,11 +190,11 @@ export class DynamicSettingsComponent implements OnInit, AfterViewInit, AfterCon
   }
 
   save() {
-    console.log('this.componentForm ==>',this.componentForm.value )
-    console.log('this.componentForm ==>',this.componentForm.valid )
-    // if (this.componentForm.invalid) {
-    //   return this.componentForm.markAsTouched()
-    // }
+
+    if (this.componentForm.invalid) {
+      this.componentForm.markAsTouched()
+      return
+    }
     const mappingData = {
       configs: this.mappingData()
     }
@@ -210,7 +208,6 @@ export class DynamicSettingsComponent implements OnInit, AfterViewInit, AfterCon
           } else {
             this.toasterService.error(res.message);
           }
-        }
-      );
+        });
   }
 }
