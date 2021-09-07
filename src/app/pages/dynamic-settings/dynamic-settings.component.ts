cefc220f140480b2565ac1e29391a4c1150d6b6d
islam-abdallah <islam@soleeeklab.com>
2021-09-07 14:01:00 +0200

    fix

--- src/app/pages/dynamic-settings/dynamic-settings.component.ts
+++ src/app/pages/dynamic-settings/dynamic-settings.component.ts
@@ -126,7 +126,7 @@ export class DynamicSettingsComponent implements OnInit, AfterViewInit, AfterCon
       order: new FormControl(data ? data.order : ''),
       type: new FormControl(data ? data.type : ''),
       name: new FormControl(data ? data.name : ''),
-      value: new FormControl(data ? data.value : '', [data.required ? Validators.required : Validators.nullValidator]),
+      value: new FormControl(data ? data.value : '', data.required == true ? [Validators.required] : []),
       options: new FormControl(data ? data.options : ''),
       group: new FormControl(data ? data.group : ''),
       slug: new FormControl(data ? data.slug : ''),
@@ -191,11 +191,11 @@ export class DynamicSettingsComponent implements OnInit, AfterViewInit, AfterCon
   }
 
   save() {
-
-    if (this.componentForm.invalid) {
-      this.componentForm.markAsTouched()
-      return
-    }
+    console.log('')
+    // if (this.componentForm.invalid) {
+    //   this.componentForm.markAsTouched()
+    //   return
+    // }
     const mappingData = {
       configs: this.mappingData()
     }
