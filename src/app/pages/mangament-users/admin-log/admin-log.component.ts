1008e3efd7854802520ad91afc6f9734e070cfc5
nadenagi <37834449+nadenagi@users.noreply.github.com>
2021-09-07 14:31:14 +0200

    admin log page

--- src/app/pages/mangament-users/admin-log/admin-log.component.ts
+++ src/app/pages/mangament-users/admin-log/admin-log.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminsService } from '@app/pages/services/admins.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-admin-log',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-log.component.css']
 })
 export class AdminLogComponent implements OnInit {
-
-  constructor() { }
+  list: any[] = [];
+  loading: boolean = false;
+  itemsPerPage: number = 20;
+  totalPages: number = 1;
+  page: 1;
+  constructor(private adminService: AdminsService, private toasterService: ToastrService, private spinner: NgxSpinnerService) {
+  }
 
   ngOnInit() {
+    this.loading = true;
+    this.adminService.getLog(this.page).subscribe((res) => {
+      if (res.code === 200) {
+        this.list = res.data;
+      } else {
+        this.toasterService.error(res.message);
+      }
+      this.loading = false;
+    })
+  }
+
+  loadData(page: any = null) {
+    this.spinner.show();
+    this.page = (page) ? page : this.page;
+    this.adminService.getLog(this.page)
+      .subscribe(
+        (res: any) => {
+          if (res.code === 200) {
+            this.list = res.data.transactions;
+            this.totalPages = res.data.total;
+          } else {
+            this.toasterService.error(res.message);
+          }
+          this.spinner.hide();
+        }
+      );
   }
 
 }
