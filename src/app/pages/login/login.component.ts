a7d6bb4f4ac41e1d86fe68c303ad579fb412a7c5
nadenagi <37834449+nadenagi@users.noreply.github.com>
2021-10-17 12:22:52 +0200

    loading in the login screen

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,53 +1,57 @@
 // import { environmentVariables } from './../../../environments/enviromentalVariables';
 import { AuthService } from "@app/shared/auth.service";
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 import { SettingService } from "../services/setting.service";
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
-
   environmentVariables;
   user: any = {
     email: "",
-    password: ""
+    password: "",
   };
   showError: Boolean;
   errorMessage = "";
+  loggingin: Boolean = false;
 
-  constructor(private router: Router,
-    private settingService:SettingService,
-    private _auth: AuthService) {
-      this.getConfig();
-     }
+  constructor(
+    private router: Router,
+    private settingService: SettingService,
+    private _auth: AuthService
+  ) {
+    this.getConfig();
+  }
 
   ngOnInit() {
-    if(this._auth.isAuthenticated()) {
+    if (this._auth.isAuthenticated()) {
       this.router.navigate(["/pages/home"]);
     }
   }
-  getConfig(){
-    this.settingService.getenvConfig().subscribe(res=>{
-     this.environmentVariables=res;
-    })
+  getConfig() {
+    this.settingService.getenvConfig().subscribe((res) => {
+      this.environmentVariables = res;
+    });
   }
   login() {
     this.showError = false;
-    this._auth.login(this.user.email, this.user.password)
+    this.loggingin = true;
+    this._auth
+      .login(this.user.email, this.user.password)
       .subscribe((response: any) => {
-        if(response.code == 200) {
+        this.loggingin = false;
+        if (response.code == 200) {
           this._auth.setToken(response.data.token);
           this._auth.setUser(response.data);
           this._auth.setPermissions();
-          this.router.navigate(["/pages/home"])
-        }else{
+          this.router.navigate(["/pages/home"]);
+        } else {
           this.showError = true;
           this.errorMessage = response.message;
         }
-      })
+      });
   }
-
 }
