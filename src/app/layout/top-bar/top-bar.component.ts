dae7b0f72ab0bdc4a8d32ae4d86b731e655c61aa
nadenagi <37834449+nadenagi@users.noreply.github.com>
2021-10-27 12:34:57 +0200

    include date in the notification stream

--- src/app/layout/top-bar/top-bar.component.ts
+++ src/app/layout/top-bar/top-bar.component.ts
@@ -1,29 +1,26 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { AuthService } from '@app/shared/auth.service';
-import { Router } from '@angular/router';
-import { SettingService } from '@app/pages/services/setting.service';
-import { NavigationEnd } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
-import { ActivationEnd } from '@angular/router';
-import 'rxjs/add/operator/startWith';
-import 'rxjs/add/operator/pairwise';
-import { Observable, interval } from 'rxjs';
-import { switchMap, startWith } from 'rxjs/operators';
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { AuthService } from "@app/shared/auth.service";
+import { Router } from "@angular/router";
+import { SettingService } from "@app/pages/services/setting.service";
+import { NavigationEnd } from "@angular/router";
+import { ActivatedRoute } from "@angular/router";
+import "rxjs/add/operator/startWith";
+import "rxjs/add/operator/pairwise";
+import { interval } from "rxjs";
+import { switchMap, startWith } from "rxjs/operators";
 
 declare var jquery: any;
 declare var $: any;
-
-
 @Component({
-  selector: 'top-bar',
-  templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.scss']
+  selector: "top-bar",
+  templateUrl: "./top-bar.component.html",
+  styleUrls: ["./top-bar.component.scss"],
 })
 export class TopBarComponent implements OnInit {
   pollingData: any;
   unread: any;
   notifToggle = false;
-  @ViewChild('notificationAudio') audioPlayerRef: ElementRef;
+  @ViewChild("notificationAudio") audioPlayerRef: ElementRef;
   notificationGroups = [];
 
   title;
@@ -33,19 +30,23 @@ export class TopBarComponent implements OnInit {
   loading: Boolean = true;
   detailsImport: any;
 
-  constructor(private auth: AuthService, private router: Router,
+  constructor(
+    private auth: AuthService,
+    private router: Router,
     private settingService: SettingService,
-    private activatedRoute: ActivatedRoute) {
-    settingService.imagesEmitter.subscribe(item => {
+    private activatedRoute: ActivatedRoute
+  ) {
+    settingService.imagesEmitter.subscribe((item) => {
       this.user = item;
-
     });
-
   }
 
   ngOnInit() {
     this.pollingData = interval(15000)
-      .pipe(startWith(0), switchMap(() => this.settingService.getNotifications()))
+      .pipe(
+        startWith(0),
+        switchMap(() => this.settingService.getNotifications())
+      )
       .subscribe((response: any) => {
         this.loading = false;
         this.notificationGroups = response.data.notifications;
@@ -59,18 +60,20 @@ export class TopBarComponent implements OnInit {
     // this.title = this.activatedRoute.snapshot.data["title"];
 
     this.router.events
-      .startWith(new NavigationEnd(0, '/', '/'))
+      .startWith(new NavigationEnd(0, "/", "/"))
       .filter((event) => event instanceof NavigationEnd)
       // .pairwise()
       .map(() => this.activatedRoute)
       .map((route) => {
-        while (route.firstChild) { route = route.firstChild; }
+        while (route.firstChild) {
+          route = route.firstChild;
+        }
         return route;
       })
-      .filter((route) => route.outlet === 'primary')
+      .filter((route) => route.outlet === "primary")
       .mergeMap((route) => route.data)
       .subscribe((event) => {
-        this.title = event['title'];
+        this.title = event["title"];
       });
 
     this.user = this.auth.getUser();
@@ -78,27 +81,26 @@ export class TopBarComponent implements OnInit {
   }
 
   clickEvent() {
-    this.settingService.markRead()
-      .subscribe(resonse => {
-        this.unread = 0;
-      });
+    this.settingService.markRead().subscribe((resonse) => {
+      this.unread = 0;
+    });
     this.notifToggle = !this.notifToggle;
   }
 
   logout() {
     this.auth.logOut();
-    this.router.navigate(['/login']);
+    this.router.navigate(["/login"]);
   }
+
   getUser() {
-    this.settingService.getNotification()
-      .subscribe((response: any) => {
-        this.user = response.data;
-        this.user.imageUrl = this.user.image;
-      });
+    this.settingService.getNotification().subscribe((response: any) => {
+      this.user = response.data;
+      this.user.imageUrl = this.user.image;
+    });
   }
+
   openPopup(data) {
     $("#importDetails").modal("show");
     this.detailsImport = data;
   }
-
 }
