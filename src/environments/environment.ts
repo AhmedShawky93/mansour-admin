51c3b7cec646e1839336ded0489219c79b25e7b8
nadenagi <37834449+nadenagi@users.noreply.github.com>
2021-11-10 16:44:03 +0200

    environement update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   api: `https://storeapi.el-dokan.com`,
   // api: `https://kareemapi.el-dokan.com`,
   // api: `https://ogawaapi.el-dokan.com`,
-  // api: `https://ogawaapi.el-dokan.com`,
+  // api: `https://admin-qatar.aljazeeraperfumes.com/`,
   production: false,
 };
