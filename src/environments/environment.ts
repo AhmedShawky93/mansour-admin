b1664ecd448a6a2717d06a630305240197e2a318
nadenagi <37834449+nadenagi@users.noreply.github.com>
2021-11-10 15:52:29 +0200

    by bass the shipment error in order details

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
-  api: `https://storeapi.el-dokan.com`,
+  // api: `https://storeapi.el-dokan.com`,
   // api: `https://kareemapi.el-dokan.com`,
   // api: `https://ogawaapi.el-dokan.com`,
+  api: `https://api-qatar.aljazeeraperfumes.com`,
   production: false,
 };
