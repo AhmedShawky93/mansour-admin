b8026dff25fdcdfa24e0055d6e7b3ef1b2ce5708
ramadanElshenawy <ramadan@el-dokan.com>
2021-12-01 13:36:09 +0200

    invoice new ui

--- src/app/pages/order-mangament/print-receipt/print-receipt.component.ts
+++ src/app/pages/order-mangament/print-receipt/print-receipt.component.ts
@@ -1,8 +1,15 @@
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-import { ActivatedRoute } from "@angular/router";
+import {
+  Component,
+  OnInit,
+} from "@angular/core";
+import {
+  ActivatedRoute,
+  Router,
+} from "@angular/router";
+
 import { OrdersService } from "@app/pages/services/orders.service";
 import { SettingService } from "@app/pages/services/setting.service";
+
 // import { environmentVariables } from '../../../../environments/enviromentalVariables';
 
 @Component({
@@ -19,6 +26,7 @@ export class PrintReceiptComponent implements OnInit {
       },
     },
   ];
+  loading = true;
   environmentVariables;
 
   item: any;
@@ -35,14 +43,17 @@ export class PrintReceiptComponent implements OnInit {
   ngOnInit() {
     this.activeRoute.params.subscribe((params) => {
       let id = params["id"];
-
       this.orderService.getOrderPrint(id).subscribe((response: any) => {
         this.order = response.data;
-        setTimeout(() => {
-          window.print();
-        }, 800);
+        this.loading = false;
+        // setTimeout(() => {
+        //   this.print();
+        // }, 800);
       });
     });
+  }
+  print() {
+    window.print();
   }
   getConfig() {
     this.settingService.getenvConfig().subscribe((res) => {
