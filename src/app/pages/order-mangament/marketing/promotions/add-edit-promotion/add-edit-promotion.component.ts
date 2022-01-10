999839a979061140e2dce3ed676375e21a94a3ec
unknown <ramadan@el-dokan.com>
2022-01-10 18:11:12 +0200

    Test push

--- src/app/pages/order-mangament/marketing/promotions/add-edit-promotion/add-edit-promotion.component.ts
+++ src/app/pages/order-mangament/marketing/promotions/add-edit-promotion/add-edit-promotion.component.ts
@@ -469,11 +469,11 @@ getAllGroups(){
     if(this.promotionForm.get("group_id").value == 'null'){
       this.promotionForm.get("group_id").setValue(null);
     }
-    if (this.promotionData) {
-      this.editPromotion();
-    } else {
+
+    this.promotionData ?
+      this.editPromotion():
       this.createPromotion();
-    }
+
   }
 
   createPromotion() {
